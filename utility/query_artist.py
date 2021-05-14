bf1626476645296d591c1a233502242623d54553
k-z-m-r <jdkazime@buffalo.edu>
2021-05-14 04:53:23 -0400

    Finding most similar artist.
    I return the most similar artist in a tuple of (name, id) so we can use this later on.  I need to write down that difflib and numpy are included down in the readme.  I also need to document this portion of the code.  It would be good to further generalize this in case it's presented in the form of a list instead of a dict.

--- utility/query_artist.py
+++ utility/query_artist.py
@@ -1,4 +1,6 @@
 from urllib import error, parse, request
+from difflib import SequenceMatcher
+from numpy import argmax
 import json
 
 from connect_to_spotify import *
@@ -11,9 +13,6 @@ from connect_to_spotify import *
 # what we attempt to do here.                        #
 ######################################################
 
-id = '7b2232f3e2364e5ebf1fd25a94abe6a3'
-secret = '1ec94311bb0c4799a46bb24a15c82387'
-token = get_access_token(id, secret)
 def get_potential_artists(search, access):
 
     '''
@@ -46,8 +45,42 @@ def get_potential_artists(search, access):
         # Otherwise, something went wrong.  This could be like
         # illegal characters (emojis) or something went wrong.
         print('Bad request!')
-        
+
         return None
 
-print(get_potential_artists('A$AP', token))
+def most_similar_artist(search, potential_artists):
 
+    '''
+        search -> the artist that the person is looking for.
+        potential_artists -> a dict of
+    '''
+    try:
+        list_artists = potential_artists['artists']['items']
+        list_artists = [(artist['name'], artist['id']) for artist in list_artists]
+
+        if len(list_artists) == 0:
+            print('Empty list!')
+            return None
+
+        elif len(list_artists) == 1:
+            return list_artists[0]
+
+        else:
+            search = search.strip()
+
+            similarity = lambda a, b: SequenceMatcher(None, a, b).ratio()
+
+            similarities = [similarity(search.lower(), artist[0].lower()) for artist in list_artists]
+            max_similarity = argmax(similarities)
+            
+            top_match = list_artists[max_similarity]
+
+            if similarities[max_similarity] < 0.80:
+                print('We could not find an accurate result.  Did you enter the right artist?')
+            
+            return top_match
+
+    except:
+        print('Data in incorrect form!')
+        return None
+    
